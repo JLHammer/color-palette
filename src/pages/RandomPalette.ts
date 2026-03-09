48a6fbf467c545cf62ef5e1bffbcc82fb4bbac94
Jens L. Hammer <jens@lindberghammer.dk>
2026-03-09 18:36:27 +0100

    Work on copy button, needs more work

--- src/pages/RandomPalette.ts
+++ src/pages/RandomPalette.ts
@@ -1,7 +1,10 @@
 import { create } from "../utils/create";
 import { set } from "../utils/set";
 
+import { ColorPalette } from "../components/ColorPalette";
+
 export const RandomPalettePage = () => {
   const RandomPaletteContainer = create("div", "random-palette-container");
+
   return RandomPaletteContainer;
 };
