db84e64061ab468047274aecbafda4d7c6f3e634
both-both <claraboth@gmail.com>
2026-03-12 14:14:03 +0100

    media queries done on footer

--- src/layout/footer.ts
+++ src/layout/footer.ts
@@ -4,16 +4,17 @@ import { set } from "../utils/set";
 export const Footer = () => {
   const footer = create(
     "footer",
-    "mt-auto flex flex-col items-center justify-end gap-1 bg-dark-gray px-2 py-1 text-center text-white",
+    "mt-auto flex flex-col items-center justify-end gap-1 bg-dark-gray px-2 pt-9 pb-5 text-center text-white",
   );
 
   const title = create("p", "text-xs");
   const author = create("p", "text-xs");
-  const credits = create("p", "text-sm");
+  const credits = create("a", "text-sm gradient-text md:text-base") as HTMLAnchorElement;
 
   title.textContent = "Colorizer Palette Generator";
-  author.textContent = "Created by Mike Montgomery";
+  author.textContent = "Created by Jens Hammer & Clara Both";
   credits.textContent = "Made possible by colormind.io palette API";
+  credits.href = "http://colormind.io/";
 
   set([title, author, credits], footer);
   return footer;
